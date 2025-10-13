7f25573250a702e8981f01bdf46f1adacad49073
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-10-13 13:55:02 +0700

    raw query test

---
+++ tests/Feature/TesRawQuery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class TesRawQuery extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("delete from categories");
+    }
+
+    public function testExample(): void
+    {
+        DB::insert("insert into categories(id, name, description, created_at) values(?,?,?,?)", [
+            'GADGET',
+            'Gadget',
+            'Gadget Category ku',
+            '2025-10-10 00:00:00'
+        ]);
+
+        $result = DB::select("select * from categories where id = ?", ['GADGET']);
+
+        self::assertEquals(1, count($result));
+        self::assertEquals('Gadget', $result[0]->name);
+    }
+}
