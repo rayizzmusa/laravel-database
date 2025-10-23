d531d7d5ae7913f7c87ae50475ceb1dfda19e824
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-10-23 21:08:27 +0700

    databse migration

---
+++ database/migrations/2025_10_23_133940_create_table_product.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->string("id", 100)->nullable(false)->primary();
+            $table->string("name", 100)->nullable(false);
+            $table->text("description")->nullable(true);
+            $table->integer("price")->nullable(false);
+            $table->string("category_id", 100)->nullable(false);
+            $table->timestamp("created_at")->useCurrent();
+            $table->foreign("category_id")->references("id")->on("categories");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
